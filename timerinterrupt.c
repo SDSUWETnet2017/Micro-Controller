c468445a66553a6ca64acbe3efd878fa6d9d72d6
Benjamin Hunt <benjamintyhunt@gmail.com>
2017-04-10 12:52:52 -0700

    Add files via upload

---
+++ timerinterrupt.c
@@ -0,0 +1,103 @@
+/* Jeremey Lee
+ *
+ * Blink LED w/ timer and interrupts
+ *
+ */
+
+#pragma config BWRP = OFF               // Boot Segment Write Protect (Disabled)
+#pragma config BSS = OFF                // Boot segment Protect (No boot flash segment)
+
+// FGS
+#pragma config GWRP = OFF               // General Segment Flash Write Protect (General segment may be written)
+#pragma config GSS0 = OFF               // General Segment Code Protect (No Protection)
+
+// FOSCSEL
+#pragma config FNOSC = FRCPLL           // Oscillator Select (Fast RC Oscillator with Postscaler and PLL (FRCDIV+PLL))
+#pragma config SOSCSRC = ANA            // SOSC Source Type (Analog Mode for use with crystal)
+#pragma config LPRCSEL = HP             // LPRC Power and Accuracy (High Power/High Accuracy)
+#pragma config IESO = ON                // Internal External Switch Over bit (Internal External Switchover mode enabled (Two-speed Start-up enabled))
+
+// FOSC
+#pragma config POSCMD = NONE            // Primary Oscillator Mode (Primary oscillator disabled)
+#pragma config OSCIOFNC = ON            // CLKO Pin I/O Function (Port I/O enabled (CLKO disabled))
+#pragma config POSCFREQ = HS            // Primary Oscillator Frequency Range (Primary Oscillator/External Clock frequency >8MHz)
+#pragma config SOSCSEL = SOSCHP         // SOSC Power Selection Configuration bits (Secondary Oscillator configured for high-power operation)
+#pragma config FCKSM = CSECME           // Clock Switching and Monitor Selection (Clock Switching and Fail-safe Clock Monitor Enabled)
+
+// FWDT
+#pragma config WDTPS = PS32768          // Watchdog Timer Postscale Select bits (1:32768)
+#pragma config FWPSA = PR128            // WDT Prescaler bit (WDT prescaler ratio of 1:128)
+#pragma config FWDTEN = SWON            // Watchdog Timer Enable bits (WDT controlled with SWDTEN bit setting)
+#pragma config WINDIS = OFF             // Windowed Watchdog Timer Disable bit (Standard WDT selected (windowed WDT disabled))
+
+// FPOR
+#pragma config BOREN = BOR3             // Brown-out Reset Enable bits (Enabled in hardware; SBOREN bit disabled)
+#pragma config PWRTEN = ON              // Power-up Timer Enable (PWRT enabled)
+#pragma config I2C1SEL = PRI            // Alternate I2C1 Pin Mapping bit (Default SCL1/SDA1 Pins for I2C1)
+#pragma config BORV = V18               // Brown-out Reset Voltage bits (Brown-out Reset at 1.8V)
+#pragma config MCLRE = ON               // MCLR Pin Enable bit (RA5 input disabled; MCLR enabled)
+
+// FICD
+#pragma config ICS = PGx1               // ICD Pin Placement Select (EMUC/EMUD share PGC1/PGD1)
+//#pragma config LVP = OFF
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+#include <xc.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <p24F16KL401.h>
+
+void main(void)
+{
+    //Set RB8 as output
+    TRISBbits.TRISB8 = 0;
+    LATBbits.LATB8 = 1;
+    
+    /*
+    T2CONbits.TMR2ON = 1; //Turn on Timer 2
+    T2CONbits.T2CKPS0 = 0; //Set prescaler to 16
+    T2CONbits.T2CKPS1 = 1;
+    PR2 = 0xFF;
+    
+    //Postscaler
+    T2CONbits.T2OUTPS3 = 1;
+    T2CONbits.T2OUTPS2 = 1;
+    T2CONbits.T2OUTPS1 = 1;
+    T2CONbits.T2OUTPS0 = 1;
+    
+    INTCON1bits.NSTDIS = 1; //Disable nested interrupts
+    INTCON2bits.INT0EP = 0; //interrupt 0 on posedge
+    IEC0bits.T2IE = 1; //Enable Timer2 interrupt
+    IFS0bits.T2IF = 0; //Clear Timer2 interrupt flag
+    */
+    
+    //turn on
+    T1CONbits.TON = 1;
+    T1CONbits.T1ECS1 = 1; //use LPRC as clock source
+    T1CONbits.T1ECS0 = 0;
+    
+    T1CONbits.TCKPS1 = 1; //prescaler to 1:256
+    T1CONbits.TCKPS0 = 1;
+    
+    T1CONbits.TCS = 0; //clock select bit, (Fosc/2) or T1ECS<1:0>
+    T1CONbits.TGATE = 0; //????
+    PR1 = 0x1EFF; //period counter
+    
+    IEC0bits.T1IE = 1; //enable timer1 interrupts
+    
+    
+    
+    
+    while (1)
+    {
+    }
+}
+
+void __attribute__((__interrupt__,no_auto_psv)) _T1Interrupt(void)
+{
+
+    LATBbits.LATB8 = ~(LATBbits.LATB8);
+    IFS0bits.T1IF = 0;
+   
+}
